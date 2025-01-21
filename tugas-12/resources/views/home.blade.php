df9243fd09e4cfbc54b54c6fe861ebd860cc9705
doppant <nnoormansyah@gmail.com>
2025-01-21 19:40:04 +0700

    Tugas 12 v1.1

---
+++ tugas-12/resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to SanberBook</title>
+</head>
+<body>
+    <header>
+        <h1>SanberBook</h1>
+    </header>
+    <main>
+        <section>
+            <h2>Social Media Developer Santai Berkualitas</h2>
+            <p>Belajar dan Berbagi agar hidup ini semakin santai berkualitas.</p>
+        </section>
+
+        <section>
+            <h2>Benefit Join di SanberBook</h2>
+            <ul>
+                <li>Mendapatkan motivasi dari sesama developer</li>
+                <li>Sharing knowledge dari para mastah Sanber</li>
+                <li>Dibuat oleh calon web developer terbaik</li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>Cara Bergabung ke SanberBook</h2>
+            <ol>
+                <li>Mengunjungi Website ini</li>
+                <li>Mendaftar di <a href="{{ route('register') }}">Form Sign Up</a></li>
+                <li>Selesai!</li>
+            </ol>
+        </section>
+    </main>
+</body>
+</html>
